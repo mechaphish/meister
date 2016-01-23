9d7a9f1c0700658ec08af662e74468934e0c94af
Yan <yans@yancomm.net>
2016-01-23 13:13:33 -0800

    update setup

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     name='meister',
     version='0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.1',
-    packages=['meister', 'meister.jobs' ],
+    packages=['meister', 'meister.schedulers' ],
     install_requires=[
     ],
     description='Master component of the Shellphish CRS.',
