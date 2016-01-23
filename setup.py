8c0373e821a8449aaf10ed6673542fae8271431a
Yan <yans@yancomm.net>
2016-01-23 12:15:35 -0800

    added setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name='meister',
+    version='0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.1',
+    packages=['meister', 'meister.jobs' ],
+    install_requires=[
+    ],
+    description='Master component of the Shellphish CRS.',
+    url='https://git.seclab.cs.ucsb.edu/cgc/miester',
+)
