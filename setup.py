6f98c64234cdb9f57bfbab64eb5921b55770af8b
Francesco Disperati <francesco@cs.ucsb.edu>
2016-01-23 18:19:28 -0800

    Package setup

--- setup.py
+++ setup.py
@@ -3,9 +3,9 @@ from distutils.core import setup
 setup(
     name='meister',
     version='0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.1',
-    packages=['meister', 'meister.schedulers' ],
-    install_requires=[
-    ],
+    packages=['meister', 'meister.schedulers', 'meister.ti' ],
+    scripts=['bin/meister'],
+    install_requires=[i.strip() for i in open('requirements.txt').readlines() if 'git' not in i],
     description='Master component of the Shellphish CRS.',
-    url='https://git.seclab.cs.ucsb.edu/cgc/miester',
+    url='https://git.seclab.cs.ucsb.edu/cgc/meister',
 )
