da19eb10beb3afae4dbd7b585e36f5f08944d052
Francesco Disperati <francesco@cs.ucsb.edu>
2016-01-24 17:39:07 -0800

    Schedule only once

--- meister/run.py
+++ meister/run.py
@@ -4,9 +4,9 @@ from .scheduler import Scheduler
 
 def run(url, username, password):
     api = API(url, username, password)
+    Scheduler().schedule("worker-test")
     while True:
         round_n = api.status()['round']
         print "[Meister] Round # %s" % round_n
         binaries = api.binaries(round_n)['binaries']
-        Scheduler().schedule("worker-test")
         sleep(3)
