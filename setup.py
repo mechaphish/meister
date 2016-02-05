be4d246eb6af72e66db78b3523ad14c35249615e
Francesco Disperati <francesco@cs.ucsb.edu>
2016-02-04 19:44:38 -0800

    Move Farnsworth API client to separate component

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     name='meister',
     version='0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.1',
-    packages=['meister', 'meister.schedulers', 'meister.ti' ],
+    packages=['meister', 'meister.schedulers'],
     scripts=['bin/meister'],
     install_requires=[i.strip() for i in open('requirements.txt').readlines() if 'git' not in i],
     description='Master component of the Shellphish CRS.',
