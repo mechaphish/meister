17c9903933eeb9cc6f46d45775ec5c2e950a2c65
Francesco Disperati <francesco@cs.ucsb.edu>
2016-02-08 18:30:33 -0800

    Meister main loop working

--- meister/run.py
+++ meister/run.py
@@ -1,5 +1,6 @@
 from time import sleep
 from os import environ as ENV
+from requests.exceptions import RequestException
 from .cgc_client.api import API as CgcAPI
 from .schedulers import AFLScheduler
 from farnsworth_client.api import API as ProfAPI
@@ -11,7 +12,20 @@ def run():
     ProfAPI.init(prof_url)
 
     while True:
-        round_n = cgc.status()['round']
-        print "[Meister] Round # %s" % round_n
-        binaries = cgc.binaries(round_n)['binaries']
+        try:
+            round_n = cgc.status()['round']
+            print "[Meister] Round # %s" % round_n
+            binaries = cgc.binaries(round_n)['binaries']
+            for binary in binaries:
+                cbn = ChallengeBinaryNode.find_or_create_by(
+                    name = binary['cbid'],
+                    blob = binary['data'],
+                    parent_id = None
+                )
+                AFLScheduler().schedule(cbn = cbn, cpus = 4, memory = 1)
+
+        except RequestException as ex:
+            print "[Meister] ERROR: %s" % type(ex)
+            pass
+
         sleep(3)
