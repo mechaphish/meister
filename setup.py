87f8b095bf8e381795d3fe21ef0e57ef10ebd3c3
Francesco Disperati <francesco@cs.ucsb.edu>
2016-02-29 16:27:43 -0800

    Bump to 0.0.1

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ with open('requirements.txt', 'r') as req_file:
     REQUIREMENTS = [r.strip() for r in req_file.readlines() if 'git' not in r]
 
 setup(name='meister',
-      version='0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.1',
+      version='0.0.1',
       packages=['meister', 'meister.schedulers'],
       scripts=['bin/meister'],
       install_requires=REQUIREMENTS,
