f63a2f9a6f432d626aacd051d1589640b21462a6
Francesco Disperati <francesco@cs.ucsb.edu>
2016-03-15 12:19:13 -0700

    Get scores and consensus evaluation data every round

--- meister/evaluators/__init__.py
+++ meister/evaluators/__init__.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-from farnsworth import Feedback
+from farnsworth import Feedback, Score, Evaluation, Team
 from meister.cgc.tierror import TiError
 import meister.log
 
@@ -12,7 +12,7 @@ class Evaluator(object):
         self._cgc = cgc
         self._round = round_
 
-    def run(self):
+    def _get_feedbacks(self):
         LOG.debug("Getting feedback")
         try:
             polls = self._cgc.getFeedback('poll', self._round.num)
@@ -28,3 +28,33 @@ class Evaluator(object):
             LOG.error("Feedback cbs error: %s", e.message)
 
         Feedback.create(polls=polls, povs=povs, cbs=cbs, round=self._round)
+
+    def _get_scores(self):
+        LOG.debug("Getting scores")
+        try:
+            scores = self._cgc.getStatus()['scores']
+        except TiError as e:
+            LOG.error("Scores error: %s", e.message)
+
+        Score.create(scores=scores, round=self._round)
+
+    def _get_consensus_evaluation(self):
+        for team_id in self._cgc.getTeams():
+            team = Team.find_or_create(team_id)
+            LOG.debug("Getting consensus evaluation for team %s", )
+            try:
+                cbs = self._cgc.getEvaluation('cb', self._round.num, team.name)
+            except TiError as e:
+                LOG.error("CBs error: %s", e.message)
+            try:
+                ids = self._cgc.getEvaluation('ids', self._round.num, team.name)
+            except TiError as e:
+                LOG.error("IDS error: %s", e.message)
+
+            Evaluation.create(cbs=cbs, ids=ids, team=team, round=self._round)
+
+
+    def run(self):
+        self._get_feedbacks()
+        self._get_scores()
+        self._get_consensus_evaluation()
