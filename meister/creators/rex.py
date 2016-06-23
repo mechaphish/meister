44f73d133c1b34ff940c59a2c3ff06a0b4adaef1
Nick Stephens <nick.d.stephens@gmail.com>
2016-06-22 19:37:51 -0700

    Rex creator only schedules the RexWorker on certain kinds of crashes

--- meister/creators/rex.py
+++ meister/creators/rex.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
+from rex import Vulnerability
 import meister.creators
 from farnsworth.models import RexJob
 
@@ -10,8 +11,16 @@ class RexCreator(meister.creators.BaseCreator):
     def jobs(self):
         for cbn in self.cbns():
             for crash in cbn.crashes:
+
+                # ignore crashes of kind null_dereference, uncontrolled_ip_overwrite, and uncontrolled_write
+                if crash.crash_kind in [Vulnerability.NULL_DEREFERENCE, Vulnerability.UNCONTROLLED_IP_OVERWRITE, \
+                        Vulnerability.UNCONTROLLED_WRITE]:
+                    continue
+
+                # TODO: in rare cases Rex needs more memory, can we try to handle cases where Rex needs upto 40G?
                 job = RexJob(cbn=cbn, payload={'crash_id': crash.id},
                              limit_cpu=1, limit_memory=10)
+
                 if not RexJob.queued(job):
                     LOG.debug("Yielding RexJob for %s with %s", cbn.id, crash.id)
                     yield job
