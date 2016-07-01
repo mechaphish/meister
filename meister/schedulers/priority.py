14ad35fbc48c224240c8cecddb1ba948beb49ca0
Kevin Borgolte <cao@cao.vc>
2016-06-30 22:04:50 -0700

    Add more debug statements

--- meister/schedulers/priority.py
+++ meister/schedulers/priority.py
@@ -78,8 +78,13 @@ class PriorityScheduler(meister.schedulers.BaseScheduler):
                 LOG.warning("Encountered a Pod that is not ready: %s", pod)
 
         job_ids_to_run = set(job.id for job in jobs_to_run)
+        LOG.debug("Jobs to run: %s", job_ids_to_run)
+
         workers_to_kill = {k: v for k, v in job_ids.items() if v not in job_ids_to_run}
+        LOG.debug("Killing: %s", workers_to_kill)
+
         job_ids_to_ignore = {v for v in job_ids.values() if v in job_ids_to_run}
+        LOG.debug("Not touching: %s", job_ids_to_ignore)
 
         # Kill workers
         for worker, job_id in workers_to_kill.items():
