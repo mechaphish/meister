e7eed7e203406cc322df362e8e0e0de080529bc3
Nick Stephens <nick.d.stephens@gmail.com>
2016-07-05 14:24:35 -0700

    Driller has priority of 100 for challenges with no crashes

--- meister/creators/driller.py
+++ meister/creators/driller.py
@@ -16,7 +16,7 @@ class DrillerCreator(meister.creators.BaseCreator):
 
             # is the fuzzer still working on mutating favorites?
             if not cbn.fuzzer_stat.pending_favs > 0:
-                LOG.info("AFL has no pending favs (old-school Driller style scheduling), scheduling Driller")
+                LOG.info("AFL has no pending favs, scheduling Driller")
                 LOG.debug("Found {} undrilled tests".format(len(cbn.undrilled_tests)))
                 for test in cbn.tests:
                     job, _ = DrillerJob.get_or_create(cbn=cbn,
@@ -26,6 +26,8 @@ class DrillerCreator(meister.creators.BaseCreator):
                                                       payload={'test_id': test.id})
                     LOG.debug("Yielding DrillerJob for %s with %s", cbn.id, test.id)
 
-                    job.priority = 30
+                    job.priority = 20
+                    if not cbn.found_crash():
+                        job.priority = 100
 
                     yield job
