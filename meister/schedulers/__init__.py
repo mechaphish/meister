4a37284de213015a529b3f286a31261f8ef1ba97
Kevin Borgolte <cao@cao.vc>
2016-07-07 16:59:36 -0700

    Use requests.exceptions instead of pykube.exceptions

--- meister/schedulers/__init__.py
+++ meister/schedulers/__init__.py
@@ -11,9 +11,9 @@ import operator
 import os
 import time
 
-import pykube.exceptions
 import pykube.http
 import pykube.objects
+import requests.exceptions
 
 import meister.log
 import meister.kubernetes as kubernetes
@@ -182,7 +182,7 @@ class KubernetesScheduler(object):
                 if pod.pending or pod.ready:
                     try:
                         LOG.debug("Pod %s is taking up resources", pod.name)
-                    except pykube.exceptions.HTTPError, e:
+                    except requests.exceptions.HTTPError, e:
                         LOG.error("Somehow failed at HTTP %s", e)
                     pods.append(pod)
                 elif pod.failed:
@@ -253,7 +253,7 @@ class KubernetesScheduler(object):
 
         try:
             pykube.objects.Pod(self.api, config).create()
-        except pykube.exceptions.HTTPError as error:
+        except requests.exceptions.HTTPError as error:
             if error.response.status_code == 409:
                 LOG.warning("Job already scheduled %s", job.id)
             else:
