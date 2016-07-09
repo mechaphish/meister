9d5f47df3909caccb67a4fa1728a947910052f5d
Francesco Disperati <francesco@cs.ucsb.edu>
2016-07-09 06:30:53 -0700

    Properly name NetworkPoll and NetworkPollSanitizer files

--- meister/creators/network_poll.py
+++ meister/creators/network_poll.py
@@ -12,7 +12,7 @@ class NetworkPollCreator(meister.creators.BaseCreator):
     def jobs(self):
         # get only unprocessed traffic files and schedule them.
         for curr_round_traffic in RawRoundTraffic.select().where(RawRoundTraffic.processed == False):
-            job, _ = NetworkPollJob.get_or_create(payload={'rrt_id': curr_round_traffic.id})
+            job, _ = NetworkPollJob.get_or_create(limit_cpu=1, limit_memory=2, payload={'rrt_id': curr_round_traffic.id})
+            job.priority = 50
             LOG.debug("Creating NetworkPollJob for %s ", curr_round_traffic.id)
-            # yield job
-        return iter(())
+            yield job
