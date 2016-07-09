0d1010b14b24b1049bf875ca0ca605211ba7616a
Aravind, Machiry <machiry@cs.ucsb.edu>
2016-07-09 03:57:46 -0700

    Finalized patch submission

--- meister/submitters/cb.py
+++ meister/submitters/cb.py
@@ -11,13 +11,20 @@ class CBSubmitter(object):
 
     def __init__(self, cgc):
         self._cgc = cgc
-        self.submitted_bins = {}
-        self.submission_history = {}
+        self.patch_submission_order = None
+        self.submission_index = 0
 
     def run(self, current_round, random_submit=False):
         # submit only in odd rounds, see FAQ163 & FAQ157
         if (current_round % 2) == 1:
-            no_submissions = True
+            to_submit_patch_type = None
+            to_submit_patch_types = None
+            if self.patch_submission_order is not None:
+                if self.submission_index >= (len(self.patch_submission_order) - 2):
+                        self.submission_index = 0
+                to_submit_patch_types = self.patch_submission_order[self.submission_index:]
+                self.submission_index += 1
+            submit_pt_index = 0
             for cbn in ChallengeBinaryNode.roots():
                 # submitted_ids = cbn.cs.ids_rules.where(IDSRule.submitted_at.is_null(True))
                 # # dont' submit CB that has IDS active
@@ -27,37 +34,32 @@ class CBSubmitter(object):
                 # if random_submit and (random.randint(0,1) == 0):
                 #     continue
 
+                if self.patch_submission_order is None or to_submit_patch_types is None:
+                    all_patch_types = map(lambda x: str(x), list(cbn.cs.cbns_by_patch_type().keys()))
+                    self.patch_submission_order = all_patch_types * 1000
+                    self.submission_index = 0
+                    to_submit_patch_types = self.patch_submission_order[self.submission_index:]
+                    self.submission_index += 1
+
+                if submit_pt_index >= len(to_submit_patch_types):
+                    submit_pt_index = 0
+
+                if submit_pt_index < len(to_submit_patch_types):
+                    to_submit_patch_type = to_submit_patch_types[submit_pt_index]
+
+                submit_pt_index += 1
+
                 for patch in cbn.unsubmitted_patches:
-                    cs_key = str(cbn.cs.name)
-                    patch_val = str(patch.patch_type)
-                    if cs_key not in self.submitted_bins:
-                        if patch_val not in self.submitted_bins.values():
-                            if (cs_key not in self.submission_history) or \
-                                    (patch_val not in self.submission_history[cs_key]):
-                                LOG.info("Submitting patch %s for %s on round %s",
+                    if to_submit_patch_type is None or to_submit_patch_type == str(patch.patch_type):
+                        LOG.info("Submitting patch %s for %s on round %s",
                                          patch.name, cbn.name, current_round)
-                                try:
-                                    self._cgc.uploadRCB(str(cbn.cs.name),
-                                                        (str(cbn.name), str(patch.blob)))
-                                    patch.submit()
-                                    no_submissions = False
-                                    self.submitted_bins[cs_key] = patch_val
-                                    # FIXME: we submit only one patch type at once
-                                    break
-                                except TiError as e:
-                                    LOG.error("Submission error: %s", e.message)
-                            else:
-                                LOG.info("Ignoring patch type %s for %s as we already submitted in previous round",
-                                         patch_val, cs_key)
-                        else:
-                            LOG.info("Ignoring patch type %s as we already submitted that for a different CS",
-                                     str(patch.patch_type))
+                        try:
+                            self._cgc.uploadRCB(str(cbn.cs.name), (str(cbn.name), str(patch.blob)))
+                            patch.submit()
+                            # FIXME: we submit only one patch type at once
+                            break
+                        except TiError as e:
+                            LOG.error("Submission error: %s", e.message)
                     else:
-                        LOG.info("Ignoring patch %s for %s as we already submitted a patch type %s", str(patch.name),
-                                 str(cbn.name), self.submitted_bins[cs_key])
-            if no_submissions:
-                for cs_name, patch_type in self.submitted_bins.iteritems():
-                    if cs_name not in self.submission_history:
-                        self.submission_history[cs_name] = set()
-                    self.submission_history[cs_name].add(patch_type)
-                self.submitted_bins = {}
+                        LOG.info("Ignoring patch %s for %s as to submit patch type is %s", str(patch.name),
+                                 str(cbn.name), str(to_submit_patch_type))
