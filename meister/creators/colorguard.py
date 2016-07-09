6435f9ead408271610b6772c9be3f0d51fa156a3
Nick Stephens <nick.d.stephens@gmail.com>
2016-07-09 02:31:18 -0700

    Colorguard jobs are only created if a TracerCache exists for the challenge

--- meister/creators/colorguard.py
+++ meister/creators/colorguard.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 import meister.creators
-from farnsworth.models import ColorGuardJob
+from farnsworth.models import ColorGuardJob, TracerCache
 
 LOG = meister.creators.LOG.getChild('colorguard')
 
@@ -9,18 +9,22 @@ class ColorGuardCreator(meister.creators.BaseCreator):
     @property
     def jobs(self):
         for cbn in self.cbns():
-            LOG.debug("For cbn %s there are %d tests", cbn.id, len(cbn.not_colorguard_traced))
-            for test in cbn.tests:
-                LOG.debug("ColorGuardJob for cbn %s, test %s being created", cbn.id, test.id)
-                job, _ = ColorGuardJob.get_or_create(cbn=cbn,
-                                                     payload={'test_id': test.id},
-                                                     limit_cpu=1,
-                                                     limit_memory=6)
 
-                job.priority = 20
-                # if the testcase came from Rex, bump up the priority by four
-                if test.job.worker == "rex":
-                    job.priority = 80
+            if cbn.tracer_cache.exists():
+                LOG.debug("Tracer cache found for %s, scheduling ColorGuard", cbn.name)
+                for test in cbn.tests:
+                    LOG.debug("ColorGuardJob for %s, test %s being created", cbn.name, test.id)
+                    job, _ = ColorGuardJob.get_or_create(cbn=cbn,
+                                                         payload={'test_id': test.id},
+                                                         limit_cpu=1,
+                                                         limit_memory=6)
 
-                LOG.debug("Yielding ColorGuardJob for %s with %s", cbn.id, test.id)
-                yield job
+                    job.priority = 20
+                    # if the testcase came from Rex, bump up the priority by four
+                    if test.job.worker == "rex":
+                        job.priority = 80
+
+                    LOG.debug("Yielding ColorGuardJob for %s with %s", cbn.id, test.id)
+                    yield job
+            else:
+                LOG.debug("No tracer caches for %s, refusing to schedule ColorGuard", cbn.name)
