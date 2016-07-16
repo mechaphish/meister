60cb8fe0aa8af50f14645645dbc04a7fac4689dd
Francesco Disperati <francesco@cs.ucsb.edu>
2016-07-15 23:16:40 -0700

    Capitalize constant

--- meister/creators/patcherex.py
+++ meister/creators/patcherex.py
@@ -17,7 +17,7 @@ class PatcherexCreator(meister.creators.BaseCreator):
     def jobs(self):
         LOG.debug("Collecting jobs")
         for cbn in self.cbns():
-            for patch_type in PatcherexJob.patch_types:
+            for patch_type in PatcherexJob.PATCH_TYPES:
                 job = PatcherexJob(cbn=cbn, payload={'patch_type': patch_type}
                                    limit_cpu=1, limit_memory=10240)
                 LOG.debug("Yielding PatcherexJob for %s", cbn.name)
