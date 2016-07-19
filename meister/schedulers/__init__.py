1ec35394caf2a74149606ed260b12b3e2b56f876
Kevin Borgolte <cao@cao.vc>
2016-07-18 18:25:07 -0700

    Add property to Jobs to restart

--- meister/schedulers/__init__.py
+++ meister/schedulers/__init__.py
@@ -84,11 +84,12 @@ class KubernetesScheduler(object):
         return ('KUBERNETES_SERVICE_HOST' not in os.environ or
                 os.environ['KUBERNETES_SERVICE_HOST'] == "")
 
-    def _kube_pod_template(self, job, restart_policy='OnFailure'):
+    def _kube_pod_template(self, job):
         name = self._worker_name(job.id)
         # FIXME
         cpu = str(job.limit_cpu) if job.limit_cpu is not None else 2
         memory = str(job.limit_memory) if job.limit_memory is not None else 4
+        restart_policy = 'OnFailure' if job.restart else 'Never'
         volumes = [{'name': 'devshm', 'emptyDir': {'medium': 'Memory'}}]
         volume_mounts = [{'name': 'devshm', 'mountPath': '/dev/shm'}]
         security_context = {}
