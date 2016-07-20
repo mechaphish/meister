2a3ec6bd6b7fe60e525913006c6cd9c540a27746
Kevin Borgolte <cao@cao.vc>
2016-07-19 19:41:13 -0700

    Add elephant brain concept

---
+++ meister/brains/elephant.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+
+import meister.brains
+
+LOG = meister.brains.LOG.getChild('elephant')
+
+
+class ElephantBrain(meister.brains.Brain):
+
+    def __init__(self):
+        pass
+
+    def _local(job):
+        # Local priorities are within a ChallengeSet:
+        # - What is more important for this CS right now? A PoV or a RCB?
+        pass
+
+    def _global(job):
+        # Global priorities are for the entire game:
+        # - How old is this CS? Must it be retired soon? Is it fresh?
+        # - How important is this CS?
+        #   - Do we have other unpatched/unexploited CS?
+        pass
+
+    def _sort(self, jobs):
+        jobs = ((j, self._global(j) * self._local(j) * p) for j, p in jobs)
+        return sorted(jobs, key=operator.itemgetter(1), reverse=True)
