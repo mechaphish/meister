924fb73e7abdc39d1d8de9560fa626856c9033a3
Kevin Borgolte <cao@cao.vc>
2016-07-19 23:34:14 -0700

    The elephant brain is a toad brain

--- meister/brains/elephant.py
+++ meister/brains/elephant.py
@@ -14,14 +14,14 @@ class ElephantBrain(meister.brains.Brain):
     def _local(job):
         # Local priorities are within a ChallengeSet:
         # - What is more important for this CS right now? A PoV or a RCB?
-        pass
+        return 1.
 
     def _global(job):
         # Global priorities are for the entire game:
         # - How old is this CS? Must it be retired soon? Is it fresh?
         # - How important is this CS?
         #   - Do we have other unpatched/unexploited CS?
-        pass
+        return 1.
 
     def _sort(self, jobs):
         jobs = ((j, self._global(j) * self._local(j) * p) for j, p in jobs)
