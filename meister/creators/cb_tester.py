846c4b14cbfa7dcbebf12e88f5199c2b230faf8d
Kevin Borgolte <cao@cao.vc>
2016-07-21 23:45:17 -0700

    Change CBTesterCreator loops to look the same

--- meister/creators/cb_tester.py
+++ meister/creators/cb_tester.py
@@ -19,19 +19,20 @@ class CBTesterCreator(meister.creators.BaseCreator):
             for patch_type in cs.cbns_by_patch_type():
                 # Get only polls for which scores have not been computed.
                 for poll in CBPollPerformance.get_untested_polls(cs, patch_type):
-                    num_tested_polls = CBPollPerformance.num_tested_polls(poll.cs, patch_type)
-                    priority = 100 if num_tested_polls < CBTesterCreator.MIN_TESTED_POLLS else 50
                     curr_cb_tester_job = CBTesterJob(cs=poll.cs, payload={'poll_id': poll.id,
                                                                           'cs_id': poll.cs.id,
                                                                           'patch_type': patch_type},
                                                      limit_cpu=20, limit_memory=4096*2)
 
+                    num_tested_polls = CBPollPerformance.num_tested_polls(poll.cs, patch_type)
+                    priority = 100 if num_tested_polls < CBTesterCreator.MIN_TESTED_POLLS else 50
+
                     LOG.debug("Yielding CBTesterJob for poll %s (patched %s)", poll.id, patch_type)
                     yield (curr_cb_tester_job, priority)
+
             # Create jobs for unpatched binary for untested polls
             for poll in CBPollPerformance.get_untested_polls(cs, None):
                 # Create job for unpatched binary
-                LOG.debug("Yielding CBTesterJob for poll %s (unpatched)", poll.id)
                 curr_cb_tester_job = CBTesterJob(cs=poll.cs, payload={'poll_id': poll.id,
                                                                       'cs_id': poll.cs.id},
                                                  limit_cpu=20, limit_memory=4096*2)
