a4ffafc82a68b7cd385bec727d6a0975e643addd
Nick Stephens <nick.d.stephens@gmail.com>
2016-07-24 16:00:04 -0700

    Now ShowmapSyncJob can be scheduled on multicbs

--- meister/creators/showmap_sync.py
+++ meister/creators/showmap_sync.py
@@ -18,16 +18,12 @@ class ShowmapSyncCreator(meister.creators.BaseCreator):
     def jobs(self):
         LOG.debug("Collecting jobs")
         for cs in self.challenge_sets():
-            if cs.is_multi_cbn:
-                LOG.warning("ShowmapSync does not support MultiCBs yet")
+            prev_round = Round.prev_round()
+            if prev_round:
+                job = ShowmapSyncJob(cs=cs, payload={"round_id": prev_round.id},
+                                     request_cpu=1, request_memory=4096,
+                                     limit_memory=8192, limit_time=10 * 60)
+                priority = 100  # We should always try to sync new testcases
 
-            else:
-                prev_round = Round.prev_round()
-                if prev_round:
-                    job = ShowmapSyncJob(cs=cs, payload={"round_id": prev_round.id},
-                                         request_cpu=1, request_memory=4096,
-                                         limit_memory=8192, limit_time=10 * 60)
-                    priority = 100  # We should always try to sync new testcases
-
-                    LOG.debug("Yielding ShowmapSyncJob for %s, round #%d", cs.name, prev_round.num)
-                    yield (job, priority)
+                LOG.debug("Yielding ShowmapSyncJob for %s, round #%d", cs.name, prev_round.num)
+                yield (job, priority)
