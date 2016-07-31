3beeb30578021e23911a84c4187e6079b3ffe845
Chris Salls <salls@cs.ucsb.edu>
2016-07-31 06:17:11 -0700

    schedule pov_fuzzers on opponents

--- meister/creators/povfuzzer1.py
+++ meister/creators/povfuzzer1.py
@@ -3,7 +3,7 @@
 
 from __future__ import absolute_import
 
-from farnsworth.models import Crash, PovFuzzer1Job
+from farnsworth.models import ChallengeSetFielding, Crash, Exploit, IDSRuleFielding, PovFuzzer1Job, PovTestResult, Team
 from peewee import fn
 
 import meister.creators
@@ -28,9 +28,56 @@ class PovFuzzer1Creator(meister.creators.BaseCreator):
                                             .order_by(fn.octet_length(Crash.blob).asc())
 
                 for priority, crash in self._normalize_sort(BASE_PRIORITY, enumerate(ordered_crashes)):
-                    job = PovFuzzer1Job(cs=cs, payload={'crash_id': crash.id},
+                    job = PovFuzzer1Job(cs=cs, payload={'crash_id': crash.id,
+                                                        'target_cs_fld': None,
+                                                        'target_ids_fld': None},
                                         request_cpu=1, limit_memory=2048,
                                         limit_time=5 * 60)
                     LOG.debug("Yielding PovFuzzer1Job for %s with crash %s priority %d",
                               cs.name, crash.id, priority)
                     yield (job, priority)
+            else:
+                # schedule pov_fuzzers against other teams
+                # we will schedule at most 3 exploits
+                # get the shortest crashes with exploits
+                longest = list(cs.exploits.join(Crash)
+                                          .where(Exploit.method == "fuzzer",
+                                                 Exploit.reliability > 0,
+                                                 Exploit.pov_type == "type1")
+                                          .order_by(fn.octet_length(Crash.blob).desc())
+                                          .limit(3)
+                                          .select(Exploit.crash))
+
+                for team in Team.opponents():
+                    target_cs_fld = ChallengeSetFielding.latest(cs, team)
+
+                    # if there is any CS fielded?
+                    if target_cs_fld is not None:
+                        target_ids_fld = IDSRuleFielding.latest(cs, team)
+
+                        # see if there are successful PoVs for this fielded CS and IDS.
+                        # if yes, no need to schedule Pov Testing Jobs
+                        pov_test_results = PovTestResult.best(target_cs_fld, target_ids_fld)
+
+                        # no results or we do not have strong PoV's?
+                        if pov_test_results is None or \
+                                pov_test_results.num_success < 3:
+
+                            # OK, we do not have any successful PoVs for the current fielded CS.
+                            # schedule 3 pov_fuzzers
+                            for exploit in longest:
+                                if target_ids_fld is not None:
+                                    target_ids_id = target_ids_fld.id
+                                else:
+                                    target_ids_id = None
+
+                                payload = {'crash_id': exploit.crash.id,
+                                           'target_cs_fld': target_cs_fld.id,
+                                           'target_ids_fld': target_ids_id}
+                                job = PovFuzzer1Job(cs=cs, payload=payload,
+                                                    request_cpu=1, limit_memory=2048,
+                                                    limit_time=5 * 60)
+                                priority = 80
+                                LOG.debug("Yielding targeted PovFuzzer1Job for %s with crash %s priority %d team %d",
+                                          cs.name, exploit.crash.id, priority, team.id)
+                                yield (job, priority)
--- meister/creators/povfuzzer2.py
+++ meister/creators/povfuzzer2.py
@@ -3,8 +3,8 @@
 
 from __future__ import absolute_import
 
-from farnsworth.models import Crash, PovFuzzer2Job
-from peewee import fn
+from farnsworth.models import ChallengeSetFielding, Crash, Exploit, IDSRuleFielding, PovFuzzer2Job, PovTestResult, Team
+from peewee import fn, SQL
 
 import meister.creators
 from .rex import Vulnerability, BASE_PRIORITY
@@ -32,3 +32,48 @@ class PovFuzzer2Creator(meister.creators.BaseCreator):
                     LOG.debug("Yielding PovFuzzer2Job for %s with crash %s priority %d",
                               cs.name, crash.id, priority)
                     yield (job, priority)
+            else:
+                # schedule pov_fuzzers against other teams
+                # we will schedule at most 3 exploits
+                # get the shortest crashes with exploits
+                longest = list(cs.exploits.join(Crash)
+                                          .where(Exploit.method == "fuzzer",
+                                                 Exploit.reliability > 0,
+                                                 Exploit.pov_type == "type2")
+                                          .order_by(fn.octet_length(Crash.blob).desc())
+                                          .limit(3)
+                                          .select(Exploit.crash))
+
+                for team in Team.opponents():
+                    target_cs_fld = ChallengeSetFielding.latest(cs, team)
+
+                    # if there is any CS fielded?
+                    if target_cs_fld is not None:
+                        target_ids_fld = IDSRuleFielding.latest(cs, team)
+
+                        # see if there are successful PoVs for this fielded CS and IDS.
+                        # if yes, no need to schedule Pov Testing Jobs
+                        pov_test_results = PovTestResult.best(target_cs_fld, target_ids_fld)
+
+                        # no results or we do not have strong PoV's?
+                        if pov_test_results is None or \
+                                pov_test_results.num_success < 3:
+
+                            # OK, we do not have any successful PoVs for the current fielded CS.
+                            # schedule 3 pov_fuzzers
+                            for exploit in longest:
+                                if target_ids_fld is not None:
+                                    target_ids_id = target_ids_fld.id
+                                else:
+                                    target_ids_id = None
+
+                                payload = {'crash_id': exploit.crash.id,
+                                           'target_cs_fld': target_cs_fld.id,
+                                           'target_ids_fld': target_ids_id}
+                                job = PovFuzzer2Job(cs=cs, payload=payload,
+                                                    request_cpu=1, limit_memory=2048,
+                                                    limit_time=5 * 60)
+                                priority = 80
+                                LOG.debug("Yielding targeted PovFuzzer2Job for %s with crash %s priority %d team %d",
+                                          cs.name, exploit.crash.id, priority, team.id)
+                                yield (job, priority)
