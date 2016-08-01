20287d30d1ff133a60ca6c8e26bda55e25c948b8
Kevin Borgolte <cao@cao.vc>
2016-08-01 02:12:12 -0700

    Fix formatting

--- meister/creators/showmap_sync.py
+++ meister/creators/showmap_sync.py
@@ -18,18 +18,17 @@ class ShowmapSyncCreator(meister.creators.BaseCreator):
     def _jobs(self):
         LOG.debug("Collecting jobs")
         # For each round see if we need to schedule a ShowmapSyncCreator
-        for rrt in RawRoundTraffic.select(RawRoundTraffic.id) \
-                .where(RawRoundTraffic.processed):
-
+        for rrt in RawRoundTraffic.select(RawRoundTraffic.id).where(RawRoundTraffic.processed):
             LOG.debug("RawRoundTraffic #%d has been processed", rrt.id)
 
             for cs in self.challenge_sets():
                 if cs.raw_round_polls.where(RawRoundPoll.raw_round_traffic == rrt).exists():
-
-                    job = ShowmapSyncJob(cs=cs, payload={"rrt_id": rrt.id},
-                                         request_cpu=1, request_memory=4096,
-                                         limit_memory=8192, limit_time=10 * 60)
-
+                    job = ShowmapSyncJob(cs=cs,
+                                         payload={"rrt_id": rrt.id},
+                                         request_cpu=1,
+                                         request_memory=4096,
+                                         limit_memory=8192,
+                                         limit_time=10 * 60)
                     priority = 100  # We should always try to sync new testcases
 
                     LOG.debug("Yielding ShowmapSyncJob for %s, rrt #%d", cs.name, rrt.id)
